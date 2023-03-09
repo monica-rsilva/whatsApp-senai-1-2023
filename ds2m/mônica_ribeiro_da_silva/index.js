afea7a95e1a21a6f16cfad510e58981f0013d614
MÔNICA RIBEIRO DA SILVA <22282167@CFP127ALUNO.LOCAL>
2023-03-09 11:22:04 -0300

    ajustes no style

--- ds2m/mônica_ribeiro_da_silva/index.js
+++ ds2m/mônica_ribeiro_da_silva/index.js
@@ -3,6 +3,7 @@
 import { contatos } from "./recursos/contatos.js"
 
 const criarCard = (contatos) => {
+
     const card = document.createElement('div')
     card.classList.add('card')
 
@@ -23,11 +24,36 @@ const criarCard = (contatos) => {
     return card
 }
 
+// const criarChat = () => {
+
+//     const cardChat = document.createElement('div')
+//     cardChat.classList.add('chat')
+
+//     const img = document.createElement('img')
+//     img.classList.add('card__image')
+//     img.src = `${contatos.image}`
+
+//     const name = document.createElement('name')
+//     name.classList.add('card__name')
+//     name.textContent = contatos.name
+
+//     const description = document.createElement('description')
+//     description.classList.add('card__description')
+//     description.textContent = contatos.description
+
+//     return cardChat
+// }
+
 const carregarContatos = () => {
     const container = document.getElementById('container')
     const cards = contatos.map(criarCard)
 
     container.replaceChildren(...cards)
+
 }
+
+// const carregarChat = () => {
+
+// }
 
 carregarContatos()
