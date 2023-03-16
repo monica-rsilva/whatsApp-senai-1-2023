d22c0d4df7603e6a44c49ac2a19202e3a80af551
monica <monica_ribeirosilva@outlook.com>
2023-03-16 03:03:39 -0300

    refazendo

--- ds2m/mônica_ribeiro_da_silva/index.js
+++ ds2m/mônica_ribeiro_da_silva/index.js
@@ -6,6 +6,7 @@ const criarCard = (contatos) => {
 
     const card = document.createElement('div')
     card.classList.add('card')
+    card.onclick = () => carregarChat()
 
     const img = document.createElement('img')
     img.classList.add('card__image')
@@ -15,45 +16,32 @@ const criarCard = (contatos) => {
     name.classList.add('card__name')
     name.textContent = contatos.name
 
-    const description = document.createElement('description')
-    description.classList.add('card__description')
-    description.textContent = contatos.description
+    // const description = document.createElement('description')
+    // description.classList.add('card__description')
+    // description.textContent = contatos.description
 
-    card.append(img, name, description)
+    card.append(img, name)
 
     return card
 }
-
-// const criarChat = () => {
-
-//     const cardChat = document.createElement('div')
-//     cardChat.classList.add('chat')
-
-//     const img = document.createElement('img')
-//     img.classList.add('card__image')
-//     img.src = `${contatos.image}`
-
-//     const name = document.createElement('name')
-//     name.classList.add('card__name')
-//     name.textContent = contatos.name
-
-//     const description = document.createElement('description')
-//     description.classList.add('card__description')
-//     description.textContent = contatos.description
-
-//     return cardChat
-// }
 
 const carregarContatos = () => {
     const container = document.getElementById('container')
     const cards = contatos.map(criarCard)
 
     container.replaceChildren(...cards)
-
 }
 
-// const carregarChat = () => {
+const carregarChat = () => {
+    const chat = document.getElementById('chat')
+    const card = criarCard()
 
-// }
+    chat.replaceChild(card)
+}
+
+
 
 carregarContatos()
+carregarChat()
+
+
